fe5b306a3169c4d0329386de2ec89ac827299651
ANPULI <al4902@nyu.edu>
2020-02-16 15:07:20 +0800

    progress bar completed

---
+++ src/components/AudioPlayer.js
@@ -0,0 +1,224 @@
+import React from 'react'
+import '../assets/css/audio-player.css'
+
+let clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+class AudioPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAutoPlay: false,
+      isPaused: true,
+      isProgressBarMouseDown: false,
+      currentTime: 0,
+      formattedCurrentTime: "00:00",
+      currentWidth: "0%",
+      duration: 0,
+      formattedDuration: "00:00",
+      bufferedWidth: "0%",
+    }
+    this.audio = null;
+    this.pbar = null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // if (! prevState.isAutoPlay) { this.setState({isAutoPlay: true}) }
+    if (this.props.musicList !== prevProps.musicList) {
+
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      isPaused: this.audio.paused,
+      isAutoPlay: ! this.audio.paused,
+    });
+  }
+
+  setAudioRef = element => {
+    this.audio = element;
+  }
+
+  setPbarRef = element => {
+    this.pbar = element;
+  }
+
+  handlePlay = event => {
+    this.audio.paused ? this.audio.play() : this.audio.pause();
+    this.setState({
+      isPaused: this.audio.paused,
+      isAutoPlay: ! this.audio.paused,
+    });
+  };
+
+  handleCanPlay = event => {
+    let duration = this.getMusicDuration();
+    let currentTime = this.getMusicCurrentTime();
+    let formattedDuration = this.getFormattedTime(duration);
+    let formattedCurrentTime = this.getFormattedTime(currentTime);
+    let currentWidth = this.getMusicCurrentWidth();
+    let bufferedWidth = this.getMusicBufferedWidth();
+    this.setState({
+      duration: duration,
+      currentTime: currentTime,
+      formattedDuration: formattedDuration,
+      formattedCurrentTime: formattedCurrentTime,
+      currentWidth: currentWidth,
+      bufferedWidth: bufferedWidth,});
+  };
+
+  handleTimeUpdate = event => {
+    let currentTime = this.getMusicCurrentTime();
+    let formattedCurrentTime = this.getFormattedTime(currentTime);
+    let currentWidth = this.getMusicCurrentWidth();
+    let bufferedWidth = this.getMusicBufferedWidth();
+    this.setState({
+      currentTime: currentTime,
+      formattedCurrentTime: formattedCurrentTime,
+      currentWidth: currentWidth,
+      bufferedWidth: bufferedWidth,
+    });
+  };
+
+  handleProgressBarClicked = event => {
+    let portion = (event.pageX - this.pbar.getBoundingClientRect().x) / this.pbar.getBoundingClientRect().width;
+    portion = clamp(portion, 0, 1);
+    let newTime = this.state.duration * portion;
+    if (this.audio) { this.audio.currentTime = newTime; }
+  };
+
+  handleProgressBarMouseDown = event => {
+    let portion = (event.pageX - this.pbar.getBoundingClientRect().x) / this.pbar.getBoundingClientRect().width;
+    portion = clamp(portion, 0, 1);
+    let tempWidth = portion * 100 + "%";
+    let tempTime = this.state.duration * portion;
+    let formattedTempTime = this.getFormattedTime(tempTime);
+    this.setState({
+      isProgressBarMouseDown: true,
+      currentWidth: tempWidth,
+      formattedCurrentTime: formattedTempTime,
+    });
+  };
+
+  handleProgressBarMouseUp = event => {
+    this.setState({
+      isProgressBarMouseDown: false,
+    });
+  };
+
+  handleProgressBarMouseMoved = event => {
+    let portion = (event.pageX - this.pbar.getBoundingClientRect().x) / this.pbar.getBoundingClientRect().width;
+    portion = clamp(portion, 0, 1);
+    let tempWidth = portion * 100 + "%";
+    let tempTime = this.state.duration * portion;
+    let formattedTempTime = this.getFormattedTime(tempTime);
+    this.setState({
+      currentWidth: tempWidth,
+      formattedCurrentTime: formattedTempTime,
+    });
+  };
+
+  getMusicName = () => {
+    return this.props.currentMusic ? this.props.currentMusic.name : "song name";
+  };
+
+  getMusicImg = () => {
+    return this.props.currentMusic ? this.props.currentMusic.imgUrl : "";
+  };
+
+  getMusicPage = () => {
+    return this.props.currentMusic ? this.props.currentMusic.infoUrl : "#"
+  };
+
+  getMusicDuration = () => {
+    return this.audio ? this.audio.duration : 0;
+  };
+
+  getMusicCurrentTime = () => {
+    return this.audio ? this.audio.currentTime : 0;
+  };
+
+  getMusicBufferedWidth = () => {
+    let length = this.audio.buffered.length
+    return (length > 0) ? 100 * this.audio.buffered.end(length-1) / this.audio.duration + "%" : "0%";
+  };
+
+  getMusicCurrentWidth = () => {
+    return this.audio ? 100 * this.state.currentTime / this.state.duration + "%" : "0%";
+  };
+
+  getFormattedTime = (time) => {
+    return new Date(time * 1000).toISOString().substring(14, 19);
+  };
+
+  getArtistName = () => {
+    return this.props.currentMusic ? this.props.currentMusic.artist.name : "artist"
+  };
+
+  getArtistPage = () => {
+    return this.props.currentMusic ? this.props.currentMusic.artist.url : "#"
+  };
+
+  render() {
+    const isProgressBarMouseDown = this.state.isProgressBarMouseDown;
+    return (
+      <div className="btmbar">
+        <div className="playbar">
+          <audio 
+            autoPlay={this.state.isAutoPlay}
+            ref={this.setAudioRef} 
+            src={this.props.currentMusic ? this.props.currentMusic.url : ""}
+            onEnded={this.props.onAudioEnded}
+            onTimeUpdate={isProgressBarMouseDown ? null : this.handleTimeUpdate} 
+            onCanPlay={this.handleCanPlay}
+          />
+          <div className="bg" />
+          <div className="wrap">
+            <div className="btns">
+              <a onClick={this.props.handlePrev} className="prv" title="上一首">上一首</a>
+              <a onClick={this.handlePlay} className={ this.state.isPaused ? "ply" : "pas" } title="播放/暂停">播放/暂停</a>
+              <a onClick={this.props.handleNext} className="nxt" title="下一首">下一首</a>
+            </div>
+            <div className="head">
+              <img src={this.getMusicImg()} />
+              <a href={this.getMusicPage()} hidefocus="true" className="mask"></a>
+            </div>
+            <div className="play">
+              <div className="j-flag words">
+                <a hidefocus="true" href={this.getMusicPage()} className="f-thide name fc1 f-fl" title={this.getMusicName()}>{this.getMusicName()}</a>
+                <span className="by f-thide f-fl">
+                  <span title={this.getArtistName()}>
+                    <a className="" href={this.getArtistPage()} hidefocus="true">{this.getArtistName()}</a>
+                  </span>
+                </span>
+              </div>
+              <div className="pbar"
+                ref={this.setPbarRef}
+                onClick={this.handleProgressBarClicked}
+                onMouseMove={isProgressBarMouseDown ? this.handleProgressBarMouseMoved : null}
+                onMouseDown={this.handleProgressBarMouseDown}
+                onMouseUp={this.handleProgressBarMouseUp}
+              >
+                <div className="barbg j-flag">
+                  <div className="rdy" style={{width: this.state.bufferedWidth}}></div>
+                  <div className="cur" style={{width: this.state.currentWidth}}>
+                    <span className="btn f-tdn f-alpha">
+                      <i></i>
+                    </span>
+                  </div>
+                </div>
+                <span className="j-flag time"><em>{this.state.formattedCurrentTime}</em> / {this.state.formattedDuration}</span>
+              </div>
+            </div>
+            <div className="oper f-fl">
+              <a hidefocus="true" data-action="like" className="icn icn-add j-flag" title="收藏">收藏</a>
+              <a hidefocus="true" data-action="share" className="icn icn-share" title="分享">分享</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AudioPlayer;
--- src/index.js
+++ src/index.js
@@ -26,13 +26,27 @@ class App extends React.Component {
           id: 494064179,
           name: "柴 鱼 の c a l l i n g【已售】",
           url: "https://music.163.com/song/media/outer/url?id=494064179.mp3",
+          infoUrl: "https://music.163.com/song?id=494064179",
+          imgUrl: "http://p2.music.126.net/ws5PkDTu_9iPCE9_XV0u0w==/109951163269801605.jpg?param=34y34",
           duration: 151792,
+          artist: {
+            id: 35846642,
+            url: "https://music.163.com/artist?id=35846642",
+            name: "幸子小姐拜托了",
+          },
         },
         {
           id: 433107530,
           name: "So Cute~",
           url: "https://music.163.com/song/media/outer/url?id=433107530.mp3",
+          infoUrl: "https://music.163.com/song?id=433107530",
+          imgUrl: "https://p2.music.126.net/yFCpD3GrgmcInAbVvVaFUg==/17999005346907556.jpg?param=34y34",
           duration: 184267,
+          artist: {
+            id: 12027465,
+            url: "https://music.163.com/artist?id=12027465",
+            name: "Lopu$",
+          },
         },
       ];
       return {
@@ -42,9 +56,9 @@ class App extends React.Component {
         musicList: musicList,
       };
     });
-  }
+  };
 
-  handleNextClick = () => {
+  handleNext = () => {
     this.setState((state, props) => {
       let newIndex = (state.currentIndex === state.maxIndex) ? 0 : state.currentIndex + 1;
       return {
@@ -52,15 +66,41 @@ class App extends React.Component {
         currentMusic: state.musicList[newIndex],
       };
     });
-  }
+  };
 
-  handlePrevClick = () => {
+  handlePrev = () => {
     this.setState((state, props) => {
       let newIndex = (state.currentIndex === 0) ? state.maxIndex : state.currentIndex - 1;
       return {
         currentIndex: newIndex,
         currentMusic: state.musicList[newIndex],
       };
+    });
+  };
+
+  handleDeleteMusic = (idx) => {
+    this.setState((state, props) => {
+      // recalc current index
+      let currentIndex = state.currentIndex;
+      console.log(idx, currentIndex)
+      if (idx < currentIndex) {
+        currentIndex -= 1;
+      }
+      // recalc music list
+      let musicList = state.musicList;
+      musicList.splice(idx, 1);
+      // recalc current music
+      let currentMusic = musicList[currentIndex];
+      console.log(currentIndex)
+      console.log(musicList)
+      console.log(currentMusic)
+      let maxIndex = state.maxIndex - 1;
+      return ({
+        currentIndex: currentIndex,
+        maxIndex: maxIndex,
+        currentMusic: currentMusic,
+        musicList: musicList,
+      });
     });
   }
 
@@ -69,14 +109,16 @@ class App extends React.Component {
       <div>
         <h1> Music Here</h1>
         <button onClick={this.loadMusicList}>Load music list</button>
-        <button onClick={this.handleNextClick}>next</button>
-        <button onClick={this.handlePrevClick}>prev</button>
+        <button onClick={this.handleNext}>next</button>
+        <button onClick={this.handlePrev}>prev</button>
+        <button value={0} onClick={() => this.handleDeleteMusic(0)}>deletemusic</button>
         <AudioPlayer 
           currentMusic={this.state.currentMusic} 
+          currentIndex={this.state.currentIndex} 
           musicList={this.state.musicList} 
-          onAudioEnded={this.handleNextClick} 
-          handleNext = {this.handleNextClick}
-          handlePrev = {this.handlePrevClick}
+          onAudioEnded={this.handleNext} 
+          handleNext = {this.handleNext} 
+          handlePrev = {this.handlePrev} 
         />
       </div>
     );
